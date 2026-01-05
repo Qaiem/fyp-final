8e7b5a7dd0b4a7c28750db75cfcd2ce0cc02388d
Qaiem <qaiem575@gmail.com>
2026-01-05 13:36:57 +0500

    CI/CD Changes

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -19,7 +19,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Use your hardcoded URL for debugging, but we can switch to env later
-const BASE_URL = "http://3.111.47.71:8800/api"; 
+// If VITE_API_URL exists (AWS), use it. Otherwise use localhost.
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8800/api"; 
 
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
